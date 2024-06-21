9d19d93b8b6e69a71d49e361170198a808226627
Kaled Dahleh <kdahleh23@gmail.com>
2024-06-20 22:00:22 -0500

    import alexnet and set it up for inference testing

--- AlexNetEval.py
+++ AlexNetEval.py
@@ -1,1 +1,9 @@
-# start
+import torch
+import torchvision.models as models
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = models.alexnet(pretrained=True)
+
+with torch.inference_mode():
+    pass
