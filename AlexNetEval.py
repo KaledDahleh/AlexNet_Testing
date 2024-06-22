7ca47bb6f9b5fea578e6d745ed3aef2dd3b4f1e7
Kaled Dahleh <kdahleh23@gmail.com>
2024-06-21 21:19:47 -0500

    AlexNetEval.py successfully retrieves RAW predictions from AlexNet model on ImageNet dataset

--- AlexNetEval.py
+++ AlexNetEval.py
@@ -1,9 +1,32 @@
 import torch
 import torchvision.models as models
+from PIL import Image
+import os # interact with the os, in our case, specifically files
 
+# bring in alexnet model with pretrained weights and biasesl; we're not looking to train the model
+model1 = models.alexnet(pretrained=True)
 device = "cuda" if torch.cuda.is_available() else "cpu"
+model1.to(device)
 
-model = models.alexnet(pretrained=True)
+# this gets all the image prep we need
+imageTransformations = models.AlexNet_Weights.IMAGENET1K_V1.transforms()
+
+imageFolderPath = "/Users/kaleddahleh/Downloads/test"
+
+model1.eval()
 
 with torch.inference_mode():
-    pass
+    listOfImageNames = os.listdir(imageFolderPath) # get the list of all image names
+    for imageName in listOfImageNames:
+        imagePath = f'{imageFolderPath}/{imageName}' # combine the folder path and image name to get the image path
+        # prepare the image for testing, these are AlexNet's documentation rules
+        image = Image.open(imagePath) # open the image
+        image = imageTransformations(image) # transform image to fit AlexNet testing documentation
+        image = image.unsqueeze(0) # add an additional layer to the image tensor, batch size, because this is what ImageNet expects [N, C, H, W]
+        image = image.to(device)
+
+        prediction = model1(image) # forward pass
+        
+        print(prediction)
+
+    
--- README.md
+++ README.md
@@ -7,5 +7,6 @@
     - ImageNet test images (13gb) can be downloaded from [this](https://www.image-net.org/challenges/LSVRC/2012/2012-downloads.php#images) link, once logged into [image-net.org](image-net.org)
 
     - pip install torch torchvision pillow
+        - you need PyTorch 1.9 or later to use "with torch.inference_mode()", otherwise, modify the code to "with torch.no_grad()"
 
     - ImageNet dataset --> found on ImageNet website, requires login to access dataset
