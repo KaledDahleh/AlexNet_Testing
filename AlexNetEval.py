40af3ad6d985bf3488f83c2a44672f49440e8b4c
Kaled Dahleh <kdahleh23@gmail.com>
2024-06-22 23:41:53 -0500

    I made a big mistake with the answer key, I finally found the correct one and everything seems to be working, needs LOTS of cleanup but it works

--- AlexNetEval.py
+++ AlexNetEval.py
@@ -5,12 +5,20 @@ import os # interact with the os, in our case, specifically files
 import torch.nn as nn
 import json
 
-# Path to your file
 classIndexPath = "/Users/kaleddahleh/Desktop/workspace/repos/AlexNet_Testing/imagenet_class_index.json"
 
 # Open and read the file
 with open(classIndexPath, 'r') as file:
     dictionaryOfClasses = json.load(file)
+
+codesAndLabels = list(dictionaryOfClasses.values())
+
+newdict = {}
+
+for codeAndLabel in codesAndLabels:
+    newdict[codeAndLabel[0]]=codeAndLabel[1]
+
+print(newdict)
 
 # convert labels file to a list
 openFile = open("classes.txt")
@@ -39,7 +47,13 @@ total = 50000
 with torch.inference_mode():
     listOfImageNames = os.listdir(imageFolderPath) # get the list of all image names
     listOfImageNames.sort()
-    listOfImageNames = listOfImageNames[19874:]
+
+
+    listOfCorrectLabelFiles = os.listdir("/Users/kaleddahleh/Desktop/workspace/repos/AlexNet_Testing/val")
+    # /Users/kaleddahleh/Desktop/workspace/repos/AlexNet_Testing/val/ILSVRC2012_val_00000001.xml
+    listOfCorrectLabelFiles.sort()
+    imageNum = 0
+
     for imageName in listOfImageNames:
         imagePath = f'{imageFolderPath}/{imageName}' # combine the folder path and image name to get the image path
         # prepare the image for testing, these are AlexNet's documentation rules
@@ -58,23 +72,48 @@ with torch.inference_mode():
 
         topProb, correspondingClass = torch.max(probabilities, dim = 0) # retrieve the highest probability score and its class
 
-        predictedLabel = listOfClassNames[correspondingClass.item()]
-        
-        correctLabel = dictionaryOfClasses[str(correspondingClass.item())][1]
-        correctLabel = correctLabel.replace("_", " ") # discrepency between labels 
+        predictedLabel = dictionaryOfClasses[str(correspondingClass.item())][1]
 
-        print(f'-----\ncorrect label:{correctLabel} --> predicted label:{predictedLabel}\nprobability: {topProb*100}%\nfilename: {imageName}')
+        correctLabelFilePath = f'/Users/kaleddahleh/Desktop/workspace/repos/AlexNet_Testing/val/{listOfCorrectLabelFiles[imageNum]}'
+
+        opennn = open(correctLabelFilePath)
+        reddd = opennn.readlines()
+        codeForCorrectLabel = reddd[13][8:17]
+
+        correctLabel = newdict[codeForCorrectLabel]
+
+        print(f'CORRECT LABEL: {newdict[codeForCorrectLabel]}')
+
+        imageNum += 1
+
+
+        
+        # get the correct label
+
+        # path /Users/kaleddahleh/Desktop/workspace/repos/AlexNet_Testing/val
+
+
+
+        # get the correct label
+
+
+        print(f'predicted label: {predictedLabel}')
+        print(imageName)
 
         if correctLabel == predictedLabel:
             print("CORRECT")
             correct+=1
         else:
-            print("NOT CORRECT")
-            incorrect+=1
+            print("INCORRECT")
+            incorrect +=1 
 
-        print(f'{correct} correct, {incorrect} incorrect')
+        print("----------------------------------------")
+
+
 
     print(correct)
     print(incorrect)
     print(total)
     accuracy = f'{100*(correct/total)}%'
+
+    
